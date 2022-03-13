21d5762b79d111c68b96d3f473f286afb256864a
Colton <allen.colton@hotmail.com>
2022-03-13 16:22:07 -0700

    refactored scss imports

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import './src/scss/style.module.scss'
 import './src/scss/_globals.scss';
 import './src/scss/_foundation.scss';
-import './src/scss/headers/_header.2.scss';
-import './src/scss/promos/_promos.7.scss';
+import './src/scss/headers/_header.5.scss';
+import './src/scss/promos/_promos.8.scss';
 import './src/scss/_intro.scss';
 import './src/scss/_interior.scss';
 import './src/scss/_mobile-menu.1.scss';
