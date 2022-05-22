502c44052ad34dd7030b3dd5a01873e0b5c36b18
Colton <allen.colton@hotmail.com>
2022-05-22 15:39:56 -0700

    updated blog search and single pages

--- search.php
+++ search.php
@@ -9,10 +9,23 @@
  * @since   Timber 0.1
  */
 
+$menu_title = '';
+if (is_archive() || is_author() || is_category() || is_home() || is_single() || is_tag() || is_search() ) {
+  $menu_title = 'blog';
+} elseif ( get_field('that_sidebar' )) {
+  $menu_title = get_field( 'that_sidebar' );
+} else {
+  $menu_title = 'about';
+}
+
 $templates = array( 'search.twig', 'archive.twig', 'index.twig' );
 
 $context          = Timber::context();
-$context['title'] = 'Search results for ' . get_search_query();
+$context['title'] = 'Search results for "' . get_search_query() . '"';
 $context['posts'] = new Timber\PostQuery();
+$context['sidebar'] = Timber::get_sidebar('sidebar.twig');
+$context['menu_title'] = $menu_title;
+$context['current_menu'] = $menu_title . '_menu';
+$context['blog_sidebar'] = Timber::get_widgets( 'blog_sidebar' );
 
 Timber::render( $templates, $context );
--- single.php
+++ single.php
@@ -10,7 +10,9 @@
  */
 
 $menu_title = '';
-if ( get_field('that_sidebar' )) {
+if (is_archive() || is_author() || is_category() || is_home() || is_single() || is_tag() || is_search() ) {
+  $menu_title = 'blog';
+} elseif ( get_field('that_sidebar' )) {
   $menu_title = get_field( 'that_sidebar' );
 } else {
   $menu_title = 'about';
@@ -19,8 +21,11 @@ if ( get_field('that_sidebar' )) {
 $context         = Timber::context();
 $timber_post     = Timber::get_post();
 $context['post'] = $timber_post;
+$context['category'] = get_the_category_list(', ');
 $context['sidebar'] = Timber::get_sidebar('sidebar.twig');
-// $context['sidebar'] = Timber::get_widgets( 'blog_sidebar' );
+$context['menu_title'] = $menu_title;
+$context['current_menu'] = $menu_title . '_menu';
+$context['blog_sidebar'] = Timber::get_widgets( 'blog_sidebar' );
 
 if ( post_password_required( $timber_post->ID ) ) {
 	Timber::render( 'single-password.twig', $context );
